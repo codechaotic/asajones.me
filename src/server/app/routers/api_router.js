094d41365fff47f475dd514517c0abe5257cebfb
codechaotic <thinkinfractals@gmail.com>
2015-09-02 04:52:39 -0400

    Create api_router specs

--- src/server/app/routers/api_router.js
+++ src/server/app/routers/api_router.js
@@ -2,16 +2,16 @@ module.exports = api_router;
 
 api_router.$inject = [
   'router',
-  //'article_router',
+//'article_router',
   'info_router'
-  //'page_router'
+//'page_router'
 ];
 
 function api_router(router, info_router ) {
   var api = new router({ prefix: '/api/v1' });
   return api
     .use('/info', info_router)
-    //.use('/article', article_router)
-  //  .use('/page', page_router)
+  //.use('/article', article_router)
+  //.use('/page', page_router)
     .routes();
 }
---
+++ src/server/app/routers/api_router_spec.js
@@ -0,0 +1,67 @@
+"use strict";
+
+var co = require('co');
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var api_router = require('./api_router')
+
+describe('module api_router', function() {
+  var spies;
+  var mock_info_router;
+  var mock_router;
+
+  beforeEach(function() {
+    spies = {
+      router: {
+        use: sinon.stub(),
+        routes: sinon.stub()
+      }
+    };
+
+    mock_info_router = 'fake info router';
+    
+    mock_router = function router() {
+      this.opts = arguments[0]
+      this.use = spies.router.use.returns(this);
+      this.routes = spies.router.routes.returns(this);
+    };
+  });
+
+  describe('exports', function() {
+    it('injects router and info_router', function() {
+      expect(api_router.$inject).to.deep.equal(['router', 'info_router']);
+    });
+
+    it('is named api_router', function() {
+      expect(api_router.name).to.equal('api_router');
+    });
+  });
+
+  describe('router', function() {
+    var res;
+    var routes;
+
+    beforeEach(function() {
+      res = api_router(mock_router, mock_info_router);
+      routes = new mock_router();
+    });
+
+    it('is a router', function() {
+      expect(res).to.be.an.instanceof(mock_router);
+    });
+
+    it('has the prefix /api/v1', function() {
+      expect(res).to.have.deep.property('opts.prefix','/api/v1')
+    });
+
+    it('mounts info_router onto /info', function() {
+      expect(spies.router.use).to.have.been.calledWith(
+        '/info',
+        mock_info_router);
+    });
+  });
+});
