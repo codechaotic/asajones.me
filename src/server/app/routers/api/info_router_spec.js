c22cebb93f3351147c1fb809c092fe853bfb8870
codechaotic <thinkinfractals@gmail.com>
2015-09-02 02:27:19 -0400

    cleanup code

--- src/server/app/routers/api/info_router_spec.js
+++ src/server/app/routers/api/info_router_spec.js
@@ -12,40 +12,51 @@ var info_router = require('./info_router')
 describe('module info_router', function() {
   var mock_router;
   var mock_pkg;
+  var spies;
   var res;
+  var routes;
 
   beforeEach(function() {
+    spies = {
+      get: sinon.stub(),
+      routes: sinon.stub()
+    }
+
     mock_router = function router() {
-      this.get = sinon.stub().returns(this);
-      this.routes = sinon.stub().returns(this);
+      this.mw = 'fakemw';
+      this.get = spies.get.returns(this);
+      this.routes = spies.routes.returns(this.mw);
     };
+
     mock_pkg = {
       version: 'tv',
       author: 'ta',
       name: 'tn'
     };
+
     res = info_router(mock_router, mock_pkg);
+    routes = new mock_router();
   });
 
   describe('exports', function() {
     it('injects router and pkg', function() {
-      expect(info_router.$inject).to.deep.equal(['router', 'pkg'])
+      expect(info_router.$inject).to.deep.equal(['router', 'pkg']);
     });
 
     it('is named info_router', function() {
-      expect(info_router.name).to.equal('info_router')
+      expect(info_router.name).to.equal('info_router');
     });
-
   });
 
   describe('result', function() {
     it('is an instance of router', function() {
-      expect(res).to.be.an.instanceof(mock_router);
+      expect(res).to.equal(routes.mw);
     });
 
     describe('endpoint \'/\'', function() {
       it('exists', function() {
-        expect(res.get).to.have.been.calledWith('/');
+        expect(spies.get).to.have.been.calledWith('/');
+        expect(spies.get).to.have.been.calledBefore(spies.routes);
       });
 
       describe('middleware', function() {
@@ -53,7 +64,7 @@ describe('module info_router', function() {
 
         beforeEach(function() {
           ctx = {};
-          var fn = co.wrap(res.get.args[0][1]);
+          var fn = co.wrap(spies.get.args[0][1]);
           fn.call(ctx, function*() {});
         });
 
@@ -62,9 +73,6 @@ describe('module info_router', function() {
           expect(JSON.parse(ctx.body)).to.deep.equal(mock_pkg);
         });
       });
-    })
-
-
-  })
-
+    });
+  });
 });
