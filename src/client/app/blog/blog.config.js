d0198d870f7ec604286c821d6158ef5e8dcbd641
codechaotic <thinkinfractals@gmail.com>
2015-09-20 13:52:15 -0400

    use ui-sref for links and create blog main state

--- src/client/app/blog/blog.config.js
+++ src/client/app/blog/blog.config.js
@@ -7,6 +7,13 @@ BlogConfig.$inject = [
 function BlogConfig($stateProvider) {
   $stateProvider
     .state('site.blog', {
+      template: require('./blog.template.html')
+    })
+    .state('site.blog.summary', {
+      url: "/blog",
+      template: require('./summary.template.html')
+    })
+    .state('site.blog.post', {
       url: "/blog/post/:id",
       controller: require('./single.controller'),
       controllerAs: 'ctrl',
